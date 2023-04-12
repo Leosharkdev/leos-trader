9e91586d2f29c088aff6ce138456220f55b7f34c
Leosharkdev <sharkdevjf@gmail.com>
2023-04-12 17:30:53 -0300

    inserindo formulario

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Leos-trader</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/cd4d884ead.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -63,6 +64,7 @@
         </section>
 
         <section class="container">
+            <img src="assets/img/gif trade.gif" class="justify-content-end " alt="" >
 
         </section>
 
@@ -108,14 +110,74 @@
         </section>
 
         <section class="container">
+        <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress2" class="form-label">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="inputZip">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>
 
         </section>
     </main>
 
-    <footer class="footer mt-auto py-3 bg-blue-800" style="background-color:var(--bs-primary-text-emphasis)">
+    <footer class="footer mt-auto py-3 bg-secondary" style="background-color:var(--bs-primary-text-emphasis)">
         <div class="container">
-            <span class="text-muted">Coloque o conteúdo do sticky footer aqui.</span>
+            <span class="text-muted"></span>
         </div>
+        <div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid iste porro iusto architecto consequatur atque facilis vero accusamus, voluptate tenetur, labore, excepturi consectetur. Error, ea ab! Animi reprehenderit voluptatibus aliquid.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, pariatur eum? Mollitia consequuntur cumque cum eligendi dolores, enim aspernatur? Nemo, at nihil ducimus commodi error repellendus consequuntur qui eaque ratione.</p>
+    </div>
+    <div class="col">
+        <div>
+     <i class="fa-brands fa-facebook fa-2xl "></i>
+</div>
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+  </div>
+</div>
     </footer>
 
 
